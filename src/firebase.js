42a1baf3bc36f1ed9c1cf968c9559d9463d6737a
Javier <cjavier.gar@gmail.com>
2023-10-05 18:31:59 -0600

    agregar variables de entorno

--- src/firebase.js
+++ src/firebase.js
@@ -10,13 +10,13 @@ import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopu
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA41ZQbfr4-iSN7dgo8uWqjloQwxBkHOrI",
-  authDomain: "contentai-3f684.firebaseapp.com",
-  projectId: "contentai-3f684",
-  storageBucket: "contentai-3f684.appspot.com",
-  messagingSenderId: "209665008154",
-  appId: "1:209665008154:web:31a5c68e9ea5f27f73e7a3",
-  measurementId: "G-GNJYQLC5FR"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
